315758915d3230a4903617238a10dfadaf516961
aminrad19942015-creator <aminrad1994.2015@gmail.com>
2026-09-17 11:09:19 +0330

    Add RTL root layout

---
+++ app/layout.tsx
@@ -0,0 +1,2 @@
+export const metadata={title:'Advisor Dashboard'};
+export default function RootLayout({children}:{children:React.ReactNode}){return <html lang="fa" dir="rtl"><body style={{margin:0}}>{children}</body></html>}
