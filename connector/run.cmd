4f158df34371801cbf4f77a4ecc455ce8e9debca
aminrad19942015-creator <aminrad1994.2015@gmail.com>
2026-09-24 14:12:47 +0330

    Add Friday safety guard to open-lead runner

--- connector/run.cmd
+++ connector/run.cmd
@@ -1,3 +1,8 @@
 @echo off
 cd /d "%~dp0"
-call npm run sync >> "%~dp0runtime\scheduled-sync.log" 2>&1
+for /f %%D in ('powershell -NoProfile -Command "(Get-Date).DayOfWeek"') do set DOW=%%D
+if /I "%DOW%"=="Friday" (
+  echo [%date% %time%] Friday - Open Leads sync skipped.>> "%~dp0runtimescheduled-sync.log"
+  exit /b 0
+)
+call npm run sync >> "%~dp0runtimescheduled-sync.log" 2>&1
